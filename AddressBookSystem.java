a57d1e59e87f68e9e3b655f7372647ab896d89cb
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-14 10:58:16 +0530

    feat:adding a single contact

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class AddressBookSystem{
  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
     System.out.println("Welcome to address book program in address book system proogram in addresss book system in main branch");
+    System.out.println("Enter First Name");
+    String firstName=sc.nextLine();
+     System.out.println("Enter last Name");
+    String lastName=sc.nextLine();
+     System.out.println("Enter address");
+    String address=sc.nextLine();
+     System.out.println("Enter city");
+    String city=sc.nextLine();
+     System.out.println("Enter state");
+    String State=sc.nextLine(); 
+     System.out.println("Enter zip");
+    int zip=sc.nextInt();
+     System.out.println("Enter phonenumber");
+   long phoneNumber=sc.nextLong();
+   sc.nextLine();
+    System.out.println("Enter email");
+    String email=sc.next();
+  Contacts contacts=new Contacts(firstName, lastName, address, city, State, zip, phoneNumber, email);
+  System.out.println("One Contact is added");
+  System.out.println(contacts);
  }
 }
 class Contacts{
@@ -21,5 +44,8 @@ class Contacts{
   this.zip=zip;
   this.phoneNumber=phoneNumber;
   this.email=email;
+    }
+    public String toString(){
+        return firstName+" "+lastName+" "+address+" "+city+" "+state+" "+zip+" "+phoneNumber+" "+email;
     }
 }
