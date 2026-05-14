beb2310b8a382a3bed50cce08812e247040f13ab
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-14 11:10:27 +0530

    feat:we can edit the contact

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -24,9 +24,12 @@ public class AddressBookSystem{
   Contacts contacts=new Contacts(firstName, lastName, address, city, State, zip, phoneNumber, email);
   System.out.println("One Contact is added");
   System.out.println(contacts);
+contacts.editContact();
+System.out.println(contacts);
  }
 }
 class Contacts{
+    public static Scanner sc=new Scanner(System.in);
     private String firstName;
     private String lastName;
     private String address;
@@ -38,6 +41,33 @@ class Contacts{
     public Contacts(String firstName,String lastName,String address,String city,String state,int zip,long phoneNumber,String email){
   this.firstName=firstName;
   this.lastName=lastName;
+  this.address=address;
+  this.city=city;
+  this.state=state;
+  this.zip=zip;
+  this.phoneNumber=phoneNumber;
+  this.email=email;
+    }
+    public void editContact(){
+ System.out.println("Enter correct First Name");
+    String firstName=sc.nextLine();
+     System.out.println("Enter correct last Name");
+    String lastName=sc.nextLine();
+     System.out.println("Enter correct address");
+    String address=sc.nextLine();
+     System.out.println("Enter correct city");
+    String city=sc.nextLine();
+     System.out.println("Enter correct state");
+    String state=sc.nextLine(); 
+     System.out.println("Enter correct zip");
+    int zip=sc.nextInt();
+     System.out.println("Enter correct phonenumber");
+   long phoneNumber=sc.nextLong();
+   sc.nextLine();
+    System.out.println("Enter correct email");
+    String email=sc.next();
+    this.firstName=firstName;
+      this.lastName=lastName;
   this.address=address;
   this.city=city;
   this.state=state;
